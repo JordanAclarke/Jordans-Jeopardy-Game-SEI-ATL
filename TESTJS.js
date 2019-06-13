29101ea9dba6552c83d9997e126a336a775b965a
Jordan Clarke <jordanclarkemba@gmail.com>
2019-06-13 15:18:57 -0400

    Console log Arrays

--- TESTJS.js
+++ TESTJS.js
@@ -1,7 +1,7 @@
 let gameBoard = document.querySelector('.game-board');
 
 for (let i = 0; i < 5; i++) {
-    let columns = document.createElement('titles');
+    let columns = document.createElement('h2');
     columns.classList.add('columns');
     gameBoard.appendChild(columns);
     columns.appendChild(document.createTextNode('Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5'));
@@ -9,6 +9,8 @@ for (let i = 0; i < 5; i++) {
 for (let i = 0; i < 5; i++) {
     let btn = document.createElement('button');
     btn.classList.add('square');
+    btn.dataset.column = i
+    btn.dataset.row = "one"
     gameBoard.appendChild(btn);
     btn.appendChild(document.createTextNode('$100'))
 }
@@ -37,10 +39,115 @@ for (let i = 0; i < 5; i++) {
     btn4.appendChild(document.createTextNode('$500'))
 }
 
+// const quizQuestions = document.querySelector('#quiz')
+// const resultsQuestions = document.querySelector('results')
+// const submitBtn = document.querySelector('submit')
+
+// function createQuiz () {
+//     const output = [];
+//     firstColumnQuestions.forEach(
+//      (currentQuestion, questionNumber)   
+//     )
+//     const answer =[];
+// }
+// function showResults() {
+
+// }
+
+const questions = {
+    one: [
+        {
+            question: 'Does this work',
+            options: [
+                {text: 'Yes it does', isCorrectAnswer: true}
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                {text: 'Hi', isCorrectAnswer: false}
+            ]
+        }
+    ]
+}
+
+// const firstColumnQuestions = [
+//     {
+//     questions: "Does This Work",
+//     answers: {
+//         a: "Yes it does",
+//         b: "No Sorry",
+//         c: "Try ahain"
+//     },
+//     correctAnswer: "a"
+// },
+// {
+//     questions: "Does This Work ",
+//     answers: {
+//         a: "Yes it does",
+//         b: "No Sorry",
+//         c: "Try ahain"
+//     },
+//     correctAnswer: "a"
+// },
+// {
+//     questions: "Does This Work ",
+//     answers: {
+//         a: "Yes it does",
+//         b: "No Sorry",
+//         c: "Try ahain"
+//     },
+//     correctAnswer: "a"
+// },
+//     {
+
+//         questions: "Does This Work ",
+//         answers: {
+//             a: "Yes it does",
+//             b: "No Sorry",
+//             c: "Try ahain"
+//         },
+//         correctAnswer: "a"
+//     },
+//     {
+//         questions: "Does This Work ",
+//         answers: {
+//             a: "Yes it does",
+//             b: "No Sorry",
+//             c: "Try ahain"
+//         },
+//         correctAnswer: "a"
+//     }
+// ]
+
+
 $(document).ready(function () {
 
     $('.square').on('click', function (evt) {
-        console.log(evt.target)
+        console.log(questions[this.dataset.row][this.dataset.column])
+        console.log(document.getElementById('results').dataset)
+
+        // var questions = [
+        //     /* Category 1 Questions */
+        //     {
+        //         prompt: 'Question 1?',
+        //         options: ['A', 'B', 'C', 'D'],
+        //         correctAnswer: 'A',
+        //     }]
+
+
+
+        // var question1 = $('.square').eq(0)
+
+        // var user = prompt("question1", "a", "b","c");
+
+        // if(user === "b") {
+        //     alert("Sorry Wrong Answer")
+        // } 
+        // return true;
+
+
+
 
         // $(this).addClass('remove');
         // $(this).remove();
