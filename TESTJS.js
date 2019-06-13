d9eac06f83408302f20fc7c37f05d99229842167
Jordan Clarke <jordanclarkemba@gmail.com>
2019-06-13 15:39:20 -0400

    Deleted some buttons

--- TESTJS.js
+++ TESTJS.js
@@ -17,140 +17,209 @@ for (let i = 0; i < 5; i++) {
 for (let i = 0; i < 5; i++) {
     let btn1 = document.createElement('button');
     btn1.classList.add('square');
+    btn1.dataset.column = i
+    btn1.dataset.row = "two"
     gameBoard.appendChild(btn1);
     btn1.appendChild(document.createTextNode('$200'))
 }
 for (let i = 0; i < 5; i++) {
     let btn2 = document.createElement('button');
     btn2.classList.add('square');
+    btn2.dataset.column = i
+    btn2.dataset.row = "three"
     gameBoard.appendChild(btn2);
     btn2.appendChild(document.createTextNode('$300'))
 }
 for (let i = 0; i < 5; i++) {
     let btn3 = document.createElement('button');
     btn3.classList.add('square');
+    btn3.dataset.column = i
+    btn3.dataset.row = "four"
     gameBoard.appendChild(btn3);
     btn3.appendChild(document.createTextNode('$400'))
 }
 for (let i = 0; i < 5; i++) {
     let btn4 = document.createElement('button');
     btn4.classList.add('square');
+    btn4.dataset.column = i
+    btn4.dataset.row = "five"
     gameBoard.appendChild(btn4);
     btn4.appendChild(document.createTextNode('$500'))
 }
 
-// const quizQuestions = document.querySelector('#quiz')
-// const resultsQuestions = document.querySelector('results')
-// const submitBtn = document.querySelector('submit')
-
-// function createQuiz () {
-//     const output = [];
-//     firstColumnQuestions.forEach(
-//      (currentQuestion, questionNumber)   
-//     )
-//     const answer =[];
-// }
-// function showResults() {
-
-// }
 
 const questions = {
     one: [
         {
-            question: 'Does this work',
+            question: 'Question 1',
             options: [
-                {text: 'Yes it does', isCorrectAnswer: true}
+                { text: 'Yes it does', isCorrectAnswer: true }
+            ]
+        },
+        {
+            question: 'Question2',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        },
+        {
+            question: 'Question 4',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        },
+        {
+            question: 'Question 5',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        },
+        {
+            question: 'Question 6',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        }
+
+    ],
+two: [
+    {
+        question: 'Question 1',
+        options: [
+            { text: 'Yes it does', isCorrectAnswer: true }
+        ]
+    },
+    {
+        question: 'Question2',
+        options: [
+            { text: 'Hi', isCorrectAnswer: false }
+        ]
+    },
+    {
+        question: 'Hey there',
+        options: [
+            { text: 'Hi', isCorrectAnswer: false }
+        ]
+    },
+    {
+        question: 'Hey there',
+        options: [
+            { text: 'Hi', isCorrectAnswer: false }
+        ]
+    },
+    {
+        question: 'Hey there',
+        options: [
+            { text: 'Hi', isCorrectAnswer: false }
+        ]
+    }
+    ],
+    three: [
+        {
+            question: 'Question 1',
+            options: [
+                { text: 'Yes it does', isCorrectAnswer: true }
+            ]
+        },
+        {
+            question: 'Question2',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
             ]
         },
         {
             question: 'Hey there',
             options: [
-                {text: 'Hi', isCorrectAnswer: false}
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
             ]
         }
+    ],
+    four: [
+        {
+            question: 'Question 1',
+            options: [
+                { text: 'Yes it does', isCorrectAnswer: true }
+            ]
+        },
+        {
+            question: 'Question2',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        }
+    ],
+    five: [
+        {
+            question: 'Question 1',
+            options: [
+                { text: 'Yes it does', isCorrectAnswer: true }
+            ]
+        },
+        {
+            question: 'Question2',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        },
+        {
+            question: 'Hey there',
+            options: [
+                { text: 'Hi', isCorrectAnswer: false }
+            ]
+        },
     ]
 }
-
-// const firstColumnQuestions = [
-//     {
-//     questions: "Does This Work",
-//     answers: {
-//         a: "Yes it does",
-//         b: "No Sorry",
-//         c: "Try ahain"
-//     },
-//     correctAnswer: "a"
-// },
-// {
-//     questions: "Does This Work ",
-//     answers: {
-//         a: "Yes it does",
-//         b: "No Sorry",
-//         c: "Try ahain"
-//     },
-//     correctAnswer: "a"
-// },
-// {
-//     questions: "Does This Work ",
-//     answers: {
-//         a: "Yes it does",
-//         b: "No Sorry",
-//         c: "Try ahain"
-//     },
-//     correctAnswer: "a"
-// },
-//     {
-
-//         questions: "Does This Work ",
-//         answers: {
-//             a: "Yes it does",
-//             b: "No Sorry",
-//             c: "Try ahain"
-//         },
-//         correctAnswer: "a"
-//     },
-//     {
-//         questions: "Does This Work ",
-//         answers: {
-//             a: "Yes it does",
-//             b: "No Sorry",
-//             c: "Try ahain"
-//         },
-//         correctAnswer: "a"
-//     }
-// ]
 
 
 $(document).ready(function () {
 
     $('.square').on('click', function (evt) {
         console.log(questions[this.dataset.row][this.dataset.column])
-        console.log(document.getElementById('results').dataset)
-
-        // var questions = [
-        //     /* Category 1 Questions */
-        //     {
-        //         prompt: 'Question 1?',
-        //         options: ['A', 'B', 'C', 'D'],
-        //         correctAnswer: 'A',
-        //     }]
+        // console.log(document.getElementById('results').dataset)
 
 
-
-        // var question1 = $('.square').eq(0)
-
-        // var user = prompt("question1", "a", "b","c");
-
-        // if(user === "b") {
-        //     alert("Sorry Wrong Answer")
-        // } 
-        // return true;
-
-
-
-
-        // $(this).addClass('remove');
-        // $(this).remove();
     });
 
 })
