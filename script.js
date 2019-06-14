24973bb2d12d17b1a1473b4128f8bc3e129b5184
Jordan Clarke <jordanclarkemba@gmail.com>
2019-06-14 13:24:55 -0400

    Commited three sets of questions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let gameBoard = document.querySelector('.game-board');
-const titlesArray = ["Sports", "World", "Computers", "Movies", "U.S. History"]
+const titlesArray = ["Sports", "World", "Technology", "Movies", "U.S. History"]
 for (let i = 0; i < 5; i++) {
     let columns = document.createElement('h2');
     columns.classList.add('categories');
@@ -18,7 +18,7 @@ for (let i = 0; i < 5; i++) {
     btn.setAttribute("id", i)
     btn.appendChild(document.createTextNode('$100'))
     gameBoard.appendChild(btn);
-    
+
 }
 for (let i = 0; i < 5; i++) {
     let btn1 = document.createElement('button');
@@ -59,46 +59,41 @@ for (let i = 0; i < 5; i++) {
 
 var question1 = [
     one = {
-            prompt: "In baseball and softball what do the initials RBI stand for?\n(a) Home-Run\n\
-            (b) Run Batted In\n(c) Free Throw",
-            answer: "b"
-        },
-        {
-            prompt: "What is the tallest building in the world?\n(a) Your apartment complex\n\
-            (b) the buju khalifa\n(c) World trade center",
-            answer: "b"
-        },
-        {
-            prompt: "Question3\n(a) AnswerC\n\
+        prompt: "In baseball and softball what do the initials RBI stand for?\n(a) Home-Run\n\(b) Run Batted In\n(c) Free Throw",
+        answer: "b"
+    },
+    {
+        prompt: "What is the tallest building in the world?\n(a) Your apartment complex\n\(b) the buju khalifa\n(c) World trade center",
+        answer: "b"
+    },
+    {
+        prompt: "On the popular social website Reddit, what does AMA stand for?\n(a) Ask me Alot\n\(b) Ask Me Anything\n(c) American Music Awards",
+        answer: "b"
+    },
+    {
+        prompt: "Question3\n(a) AnswerD\n\
             (b) purple\n(c) orange",
-            answer: "c"
-        },
-        {
-            prompt: "Question3\n(a) AnswerD\n\
+        answer: "a"
+    },
+    {
+        prompt: "Question3\n(a) AnswerE\n\
             (b) purple\n(c) orange",
-            answer: "a"
-        },
-        {
-            prompt: "Question3\n(a) AnswerE\n\
-            (b) purple\n(c) orange",
-            answer: "B"
-        }
-    ]
-    var question2 = [
+        answer: "B"
+    }
+]
+var question2 = [
     two = {
         prompt: "In basketball,what is the term used when a player reaches double digits in three of five statististical categories?\n(a) Double Double\n\
         (b) Triple Double\n(c) Triple Jump",
         answer: "b"
     },
     {
-        prompt: "Which famous toy manufacturer is also the world’s largest tire manufacturer by units produced?\n(a) Lego\n\
-        (b) Marvel\n(c) Haisbro",
+        prompt: "Which famous toy manufacturer is also the world’s largest tire manufacturer by units produced?\n(a) Lego\n\(b) Marvel\n(c) Haisbro",
         answer: "a"
     },
     {
-        prompt: "Question3\n(a) AnswerA\n\
-        (b) purple\n(c) orange",
-        answer: "c"
+        prompt: "When referring to cables used to transmit audio/video, what does HDMI stand for?\n(a) High-Definition Multimedia Interface\n\(b) High-Definition media Interface\n(c) High-Definition Multimedia Interface",
+        answer: "a"
     },
     {
         prompt: "Question3\n(a) AnswerD\n\
@@ -113,19 +108,16 @@ var question1 = [
 ]
 var question3 = [
     three = {
-        prompt: "Which water sport is the official state individual sport of Hawaii?\n(a) Surfing\n\
-        (b) Diving\n(c) Boating",
+        prompt: "Which water sport is the official state individual sport of Hawaii?\n(a) Surfing\n\(b) Diving\n(c) Boating",
         answer: "a"
     },
     {
-        prompt: "The island of Saipan is a commonwealth of which country?\n(a) United States\n\
-        (b) France\n(c) Great Britain",
+        prompt: "The island of Saipan is a commonwealth of which country?\n(a) United States\n\(b) France\n(c) Great Britain",
         answer: "a"
     },
     {
-        prompt: "Question3\n(a) AnswerA\n\
-        (b) purple\n(c) orange",
-        answer: "c"
+        prompt: "What do the letters CPU stand for when referring to the “brains” of a computer?\n(a) central processing unit\n\(b) central protective unit\n(c) central preventative unit",
+        answer: "a"
     },
     {
         prompt: "Question3\n(a) AnswerD\n\
@@ -140,18 +132,15 @@ var question3 = [
 ]
 var question4 = [
     four = {
-        prompt: "The NBA’s Memphis Grizzlies formerly played in what Canadian city?\n(a) Vancouver\n\
-        (b) Atlanta\n(c) Montreal",
+        prompt: "The NBA’s Memphis Grizzlies formerly played in what Canadian city?\n(a) Vancouver\n\(b) Atlanta\n(c) Montreal",
         answer: "a"
     },
     {
-        prompt: "What is the official language of the Canadian province Quebec?\n(a) English\n\
-        (b) French\n(c) Spanish",
+        prompt: "What is the official language of the Canadian province Quebec?\n(a) English\n\(b) French\n(c) Spanish",
         answer: "c"
     },
     {
-        prompt: "What “King” of golf lent his name to a mixture of iced tea and lemonade?\n(a) Tiger Woods\n\
-        (b) Arnold Palmer\n(c) Arnold Palmer",
+        prompt: "What does the acronym USB stand for when referring to a computer port?\n(a) universal series battery\n\(b) universal series bus\n(c) universal serial bus",
         answer: "c"
     },
     {
@@ -165,62 +154,59 @@ var question4 = [
         answer: "B"
     },
 ]
-    var question5 = [
-        four = {
-            prompt: "What “King” of golf lent his name to a mixture of iced tea and lemonade?\n(a) Tiger Woods\n\
-            (b) Arnold Palmer\n(c) Arnold Palmer",
-            answer: "c"
-        },
-        {
-            prompt: "What is the official language of the Canadian province Quebec?\n(a) English\n\
-            (b) French\n(c) Spanish",
-            answer: "c"
-        },
-        {
-            prompt: "What “King” of golf lent his name to a mixture of iced tea and lemonade?\n(a) Tiger Woods\n\
-            (b) Arnold Palmer\n(c) Arnold Palmer",
-            answer: "c"
-        },
-        {
-            prompt: "Question3\n(a) AnswerD\n\
+var question5 = [
+    four = {
+        prompt: "What “King” of golf lent his name to a mixture of iced tea and lemonade?\n(a) Tiger Woods\n\(b) Arnold Palmer\n(c) Arnold Palmer",
+        answer: "c"
+    },
+    {
+        prompt: "What is the official language of the Canadian province Quebec?\n(a) English\n\(b) French\n(c) Spanish",
+        answer: "c"
+    },
+    {
+        prompt: "Regarding data storage, what does the acronym SSD stand for?\n(a) solid safe drive\n\(b) solid steak drive\n(c) solid state drive",
+        answer: "c"
+    },
+    {
+        prompt: "Regarding data storage, what does the acronym SSD stand for?\n(a) solid safe drive\n\
+            (b) solid steak drive\n(c) solid state drive",
+        answer: "c"
+    },
+    {
+        prompt: "Question3\n(a) AnswerE\n\
             (b) purple\n(c) orange",
-            answer: "a"
-        },
-        {
-            prompt: "Question3\n(a) AnswerE\n\
-            (b) purple\n(c) orange",
-            answer: "B"
-        },
+        answer: "B"
+    },
 ]
 
 var row1 = $('.square');
 
 row1.on('click', function (evt) {
     var tile = evt.target.id;
-  console.log(evt.target)
-  var prompt = question1[tile].prompt;
-  alert(prompt);
-  var response = window.prompt(question1[tile].prompt);
-  if (response === question1[tile].answer) {
-    alert('correct')
-  } else {
-      alert('wrong')
-  } return;
+    console.log(evt.target)
+    var prompt = question1[tile].prompt;
+    alert(prompt);
+    var response = window.prompt(question1[tile].prompt);
+    if (response === question1[tile].answer) {
+        alert('correct')
+    } else {
+        alert('wrong')
+    } return;
 })
 
 var row2 = $('.square1');
 
 row2.on('click', function (evt) {
     var tile = evt.target.id;
-  console.log(evt.target)
-  var prompt = question2[tile].prompt;
-  alert(prompt);
-  var response = window.prompt(question2[tile].prompt);
-  if (response === question2[tile].answer) {
-    alert('correct')
-  } else {
-      alert('wrong')
-  } return;
+    console.log(evt.target)
+    var prompt = question2[tile].prompt;
+    alert(prompt);
+    var response = window.prompt(question2[tile].prompt);
+    if (response === question2[tile].answer) {
+        alert('correct')
+    } else {
+        alert('wrong')
+    } return;
 })
 
 
@@ -228,30 +214,30 @@ var row3 = $('.square2');
 
 row3.on('click', function (evt) {
     var tile = evt.target.id;
-  console.log(evt.target)
-  var prompt = question3[tile].prompt;
-  alert(prompt);
-  var response = window.prompt(question3[tile].prompt);
-  if (response === question3[tile].answer) {
-    alert('correct')
-  } else {
-      alert('wrong')
-  } return;
+    console.log(evt.target)
+    var prompt = question3[tile].prompt;
+    alert(prompt);
+    var response = window.prompt(question3[tile].prompt);
+    if (response === question3[tile].answer) {
+        alert('correct')
+    } else {
+        alert('wrong')
+    } return;
 })
 
 var row4 = $('.square3');
 
 row4.on('click', function (evt) {
     var tile = evt.target.id;
-  console.log(evt.target)
-  var prompt = question4[tile].prompt;
-  alert(prompt);
-  var response = window.prompt(question3[tile].prompt);
-  if (response === question3[tile].answer) {
-    alert('correct')
-  } else {
-      alert('wrong')
-  } return;
+    console.log(evt.target)
+    var prompt = question4[tile].prompt;
+    alert(prompt);
+    var response = window.prompt(question3[tile].prompt);
+    if (response === question3[tile].answer) {
+        alert('correct')
+    } else {
+        alert('wrong')
+    } return;
 })
 
 
@@ -259,19 +245,19 @@ var row5 = $('.square4');
 
 row5.on('click', function (evt) {
     var tile = evt.target.id;
-  console.log(evt.target)
-  var prompt = question5[tile].prompt;
-  alert(prompt);
-  var response = window.prompt(question4[tile].prompt);
-  if (response === question4[tile].answer) {
-    alert('correct')
-  } else {
-      alert('wrong')
-  } return;
+    console.log(evt.target)
+    var prompt = question5[tile].prompt;
+    alert(prompt);
+    var response = window.prompt(question4[tile].prompt);
+    if (response === question4[tile].answer) {
+        alert('correct')
+    } else {
+        alert('wrong')
+    } return;
 })
 // })
-    
-    
+
+
     //     var score = 0;
     //     for(var i = 0; i < question.length; i++) {
     //         var response = window.prompt(question[i].prompt);
@@ -286,7 +272,7 @@ row5.on('click', function (evt) {
     // })
 
 
-    
+
 
 
 
@@ -294,15 +280,15 @@ row5.on('click', function (evt) {
 
 //     $('.square').on('click', function (evt) {
 //         prompt(questions[this.dataset.row][this.dataset.column])
-       
-        
+
+
         // console.log(document.getElementById('results').dataset)
-        
+
         // let questionTileInfo = evt.target.id;
         // event.target.innerHtml = questions[questionTileInfo]
 
     // });
 
-  
-       
+
+
     // })
